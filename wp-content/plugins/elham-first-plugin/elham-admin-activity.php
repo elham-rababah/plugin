6f2a93ba54c4be1fc5e2329a7789108f21514f62
Elham <elham.a.rababah@gmail.com>
2017-10-11 23:21:39 +0300

    add first custom post

--- wp-content/plugins/elham-first-plugin/elham-admin-activity.php
+++ wp-content/plugins/elham-first-plugin/elham-admin-activity.php
@@ -8,3 +8,36 @@
 	* Author URI:https://github.com/elham-rababah
 	* License: GPL2 
 */
+
+// Register the Services Post Type
+ 
+function elham_register_servises_post() {
+	$labels = array(
+		'name' => 'Services',
+	    'singular_name' => 'Service',
+	    );
+
+    $args = array(
+    	'labels' => $labels,
+       	'hierarchical' => true,
+       	'description' => 'Services filterable by genre',
+       	'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'page-attributes' ),
+       	'taxonomies' => array( 'genres' ),
+	    'public' => true,
+	    'show_ui' => true,
+       	'show_in_menu' => true,
+       	'menu_position' => 5,
+       	'menu_icon' => 'dashicons-format-audio',
+       	'show_in_nav_menus' => true,
+       	'publicly_queryable' => true,
+       	'exclude_from_search' => false,
+       	'has_archive' => true,
+       	'query_var' => true,
+       	'can_export' => true,
+       	'rewrite' => true,
+       	'capability_type' => 'post'
+       	);
+    register_post_type( 'Services', $args );
+}
+
+add_action( 'init', 'elham_register_servises_post' );
